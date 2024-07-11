351e4d5490d317bcf77818b8fc8df96141fc35a6
Iammm <138758217+ilhaaam24@users.noreply.github.com>
2024-07-11 07:06:37 +0700

    add routing siswa

---
+++ Installing/app/Http/SiswaController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SiswaController extends Controller
+{
+    //
+    function index(){
+        return "<h1>Saya Siswa dari Controller</h1>";
+    }
+    function detail($id){
+        return "<h1>Saya Siswa dari controller dengan ID $id</h1>";
+    }
+}
